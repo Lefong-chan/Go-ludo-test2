8abafd78f047e8e9fb9033272d7224cb25570d64
Lefong-chan <ericfongchan1@gmail.com>
2026-09-01 19:46:50 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    legacy({
+      targets: [
+        'Android >= 5',
+        'ChromeAndroid >= 49',
+        'Chrome >= 49',
+        'iOS >= 10',
+        'Safari >= 10',
+        'Firefox >= 45',
+        'Samsung >= 5',
+        'not IE 11',
+      ],
+      additionalLegacyPolyfills: ['whatwg-fetch'],
+    }),
+  ],
 })
